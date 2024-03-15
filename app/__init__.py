4e4268d80a4ab3f0c069e7290379b3e625ba028d
Dusan Rangelov <dusanrangelov@yahoo.com>
2024-03-15 23:14:21 +0100

    Feature: Sidebar hero

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from config import Config
 from flask import Flask
 from flask_bootstrap import Bootstrap
+import os
 
 bootstrap = Bootstrap()
 
 def create_app(config_class=Config):
 
-    
 
     app = Flask(__name__)
     app.config.from_object(Config)
+    
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,15 @@
 from app.main import bp
 from flask import render_template
 
+images = {
+    'bg':'static/img/bg.jpg',
+    'project_1': 'static/img/projects/1.jpg',
+    'project_2': 'img/projects/2.jpg',
+    'project_1': 'img/projects/3.jpg',
+    'project_2': 'img/projects/4.jpg',
+}
+
 @bp.route('/index',methods=['GET','POST'])
 @bp.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', images=images)
